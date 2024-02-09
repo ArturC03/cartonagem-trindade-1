2becea46ae28a52a421ab714b94b873c9c858efa
Hugo Almeida <hugo2006almeida2006@gmail.com>
2024-02-09 14:58:28 +0000

    Updated batch so it doesnt show CMD (test)

--- tools/createTasks.bat
+++ tools/createTasks.bat
@@ -7,16 +7,16 @@ REM Get the current hour
 for /f "tokens=1 delims=:" %%a in ("%time%") do set CURRENT_HOUR=%%a
 
 REM Create MINUTE task
-schtasks /Create /SC MINUTE /TN "Agendamentos Minuto a Minuto" /TR "\"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" MINUTE"
+schtasks /Create /SC MINUTE /TN "Agendamentos Minuto a Minuto" /TR "cmd /c start /B \"\" \"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" MINUTE"
 
 REM Create HOURLY task
-schtasks /Create /SC HOURLY /TN "Agendamentos Horários" /TR "\"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" HOURLY" /ST %CURRENT_HOUR%:01
+schtasks /Create /SC HOURLY /TN "Agendamentos Horários" /TR "cmd /c start /B \"\" \"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" HOURLY" /ST %CURRENT_HOUR%:01
 
 REM Create DAILY task
-schtasks /Create /SC DAILY /TN "Agendamentos Diários" /TR "\"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" DAILY" /ST 00:01
+schtasks /Create /SC DAILY /TN "Agendamentos Diários" /TR "cmd /c start /B \"\" \"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" DAILY" /ST 00:01
 
 REM Create WEEKLY task
-schtasks /Create /SC WEEKLY /TN "Agendamentos Semanais" /TR "\"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" WEEKLY" /ST 00:01
+schtasks /Create /SC WEEKLY /TN "Agendamentos Semanais" /TR "cmd /c start /B \"\" \"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" WEEKLY" /ST 00:01
 
 REM Create MONTHLY task
-schtasks /Create /SC MONTHLY /TN "Agendamentos Mensais" /TR "\"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" MONTHLY" /ST 00:01
+schtasks /Create /SC MONTHLY /TN "Agendamentos Mensais" /TR "cmd /c start /B \"\" \"%PHP_PATH%\" \"C:\xampp\htdocs\cartonagem-trindade\scheduled.php\" MONTHLY" /ST 00:01
