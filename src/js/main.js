861c62f7e360ae99e376442d4b5b10c7f85a53e6
Hugo Almeida <hugo2006almeida2006@gmail.com>
2024-05-13 13:11:48 +0100

    Done Theme Saving between Pages

--- src/js/main.js
+++ src/js/main.js
@@ -38,3 +38,46 @@
 //         }
 //     }
 // }
+
+$(document).ready(function() {
+    // var fullscreen = document.documentElement;
+    // var isFullscreen = localStorage.getItem('fullscreen') === 'true';
+
+    // if (isFullscreen) {
+    //     if (fullscreen.requestFullscreen) {
+    //         fullscreen.requestFullscreen();
+    //     } else if (fullscreen.webkitRequestFullscreen) {
+    //         fullscreen.webkitRequestFullscreen();
+    //     } else if (fullscreen.msRequestFullscreen) {
+    //         fullscreen.msRequestFullscreen();
+    //     }
+    // } else {
+    //     if (document.exitFullscreen) {
+    //         document.exitFullscreen();
+    //     } else if (document.webkitExitFullscreen) {
+    //         document.webkitExitFullscreen();
+    //     } else if (document.msExitFullscreen) {
+    //         document.msExitFullscreen();
+    //     }
+    // }
+
+    var theme = $('#theme');
+    var isDark = localStorage.getItem('theme');
+    console.log(isDark);
+    if (isDark === "true") {
+        theme.prop('checked', true);
+    } else {
+        theme.prop('checked', false);
+    }
+
+    theme.change(function() {
+        console.log('----------------');
+        if (localStorage.getItem('theme') === "false") {
+            console.log('true');
+            localStorage.setItem('theme', "true");
+        } else {
+            console.log('false');
+            localStorage.setItem('theme', "false");
+        }
+    });
+});
