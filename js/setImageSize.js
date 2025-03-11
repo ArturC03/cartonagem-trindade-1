2bce49da162c5dbc30c1f0e9e1a2fffc4c15725c
ArturC03 <arturvicentecruz@proton.me>
2025-03-11 16:31:26 +0000

    A ver se isto funciona

--- js/setImageSize.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var svgElement = document.querySelector('svg');
-    if (svgElement) {
-        var svgWidth = svgElement.clientWidth;
-        var svgHeight = svgElement.clientHeight;
-        document.getElementById('size_x').value = svgWidth;
-        document.getElementById('size_y').value = svgHeight;
-    }
-});
--- logout.php
+++
@@ -1,5 +0,0 @@
-<?php
-include('config.inc.php');
-unset($_SESSION['username']);
-header("Location: home.php");
-?>
